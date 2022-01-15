2c8fa18a9b3f4f80517c94a780085d93b57e3b15
Aleksandra Rudenko <rudenkoay8@gmail.com>
2022-01-15 21:08:35 +0300

    OOP_2

--- src/main/java/OOP/Test.java
+++ src/main/java/OOP/Test.java
@@ -4,14 +4,20 @@ public class Test {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.eat();
-        animal.sleep();
         Cat cat = new Cat();
         cat.eat();
         Dog dog = new Dog();
         dog.eat();
         dog.bark();
-        cat.sleep();
         dog.showName();
         cat.showName();
+
+        //Вызов полиморфизма
+        testPolimorfizm(dog);
+        testPolimorfizm(cat);
+        testPolimorfizm(animal);
+    }
+    public static void testPolimorfizm(Animal animal){ //Полиморфизм
+        animal.sleep();
     }
 }
